86dde111dddd73c8b70964ccd8b94d19f54046f5
Arthur <arthur.p.wright@gmail.com>
2015-06-24 23:06:03 -0400

    Adding more dependencies to app.js. Altered user routes

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ var app = require('express')();
 var exphbs  = require('express-handlebars');
 var http = require('http').Server(app);
 var io = require('socket.io')(http)
-var shows = require('./routes/shows');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var shows = require('./routes/shows');
+var users = require('./routes/users');
 
 app.use(morgan('dev'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'application'}));
 app.set('view engine', 'handlebars');
 
-
-
 app.get('/', function(req, res){
   res.render('home')
 });
 
-
-
 app.use(shows);
+app.use(users);
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/users/new', function(req, res) {
+  res.render('users/new');
+});
+
+module.exports = router;
