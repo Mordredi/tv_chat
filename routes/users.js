f81085d7547679351dea01b2041dd6c33473057f
Arthur <arthur.p.wright@gmail.com>
2015-06-24 23:41:30 -0400

    Added model for user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/users/new', function(req, res) {
+router.get('/register', function(req, res) {
   res.render('users/new');
 });
 
